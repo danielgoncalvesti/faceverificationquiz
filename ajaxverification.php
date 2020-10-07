1d689651a2faeef50c06eccf679176a75472c0fd
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2020-10-07 18:42:52 -0300

    dropbox api was added to the project

--- ajaxverification.php
+++ ajaxverification.php
@@ -29,12 +29,18 @@ define('NO_DEBUG_DISPLAY', true);
 
 require_once(__DIR__ . '/../../../../config.php');
 
+require 'vendor/autoload.php';
+use Kunnu\Dropbox\Dropbox;
+use Kunnu\Dropbox\DropboxApp;
+use Kunnu\Dropbox\DropboxFile;
+
 defined('MOODLE_INTERNAL') || die;
 
 require_once("$CFG->libdir/gdlib.php");
 $PAGE->set_url('/mod/quiz/accessrule/faceverificationquiz/upload.php');
 
 require_login(get_site(), true, null, true, true);
+$file = required_param('file', PARAM_RAW);
 $sessionid = required_param('sesskey', PARAM_RAW);
 $facevalues = required_param('descriptor', PARAM_RAW);
 $euclidean_distance = required_param('euclidean_distance', PARAM_RAW);
@@ -50,11 +56,50 @@ echo $OUTPUT->header(); // Send headers.
 if (!confirm_sesskey($sessionid)) {
     $array['errors'][] = get_string('failed:sesskey', 'faceverificationquiz');
 }
-print_r($array['errors']);
 
 if (empty($array['errors'])) {
 
+    if (stristr($file, 'base64,')) {
+        // Convert webrtc.
+        $file = explode('base64,', $file);
+        $file = end($file);
+    }
+
+    // Decode.
+    $file = base64_decode($file);
+
+    if (empty($file)) {
+        $array['errors'][] = get_string('failed', 'faceverificationquiz');
+        die(json_encode($array));
+    }    
+
     $context = context_user::instance($USER->id, MUST_EXIST);
+
+    $tempfile = tempnam(sys_get_temp_dir(), 'faceverificationquiz');
+    file_put_contents($tempfile, $file);
+
+    $dropboxFile = new DropboxFile($tempfile);
+
+    //Configure Dropbox Application
+    $app = new DropboxApp("9d8ukwyihgvpmyl", "00v4mdt2xc5zpj4", "ysIiD9OB6K0AAAAAAAAAAa5mO0TRGhL8j7ouX6ORHiG2YabMxG-2m2jyDypS-bMQ");
+
+    //Configure Dropbox service
+    $dropbox = new Dropbox($app);
+
+    //Check if the folder exists
+    // if($dropbox->listFolder("/teste"))
+    // $dropboxFolder = $dropbox->listFolder("/teste");
+    // $searchResults = $dropbox->search("/course", "2", ['start' => 0, 'max_results' => 5]);
+    // echo $searchResults;
+    // print_r($searchResults);
+
+    // $folder = $dropbox->createFolder("/MyFolder1");
+    // $dropboxPathFolder = "/" . $courseid . "/" . $quizid . "/";
+    $file = $dropbox->upload($dropboxFile, "/MyFolder1/foto1.png", ['autorename' => true]);
+
+
+    // // $client->createFolder("/teste");
+    // $client->upload($tempfile, 'teste', $mode = 'add');
 
     $faceverification = new stdClass();
     $faceverification->username = $USER->username;

d77665e2e4f026db19b903a6942f6f3da3201957
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2021-05-01 18:11:05 -0300

    the version file was updated

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_faceverificationquiz';
-$plugin->release = '1.1.8';
-$plugin->version = 2021032313;
+$plugin->release = '1.1.9';
+$plugin->version = 2021032314;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_BETA;
